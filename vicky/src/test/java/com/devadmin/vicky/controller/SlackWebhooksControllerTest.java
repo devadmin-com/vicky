b417b7a328fd0c398f35e308ce0a5e38a2bfca54
Zhirayr Gumruyan <gumruyanzh@gmail.com>
2018-11-30 12:15:29 +0400

    Adding some test methods without implementation.

---
+++ vicky/src/test/java/com/devadmin/vicky/controller/SlackWebhooksControllerTest.java
@@ -0,0 +1,13 @@
+package com.devadmin.vicky.controller;
+
+import org.junit.Test;
+
+public class SlackWebhooksControllerTest {
+
+  /**
+   * check is null received jira event
+   */
+  @Test
+  public void incomingIssueEventTest() {
+  }
+}
---
+++ vicky/src/test/java/com/devadmin/vicky/service/SlackWebhookServiceTest.java
@@ -0,0 +1,76 @@
+package com.devadmin.vicky.service;
+
+import org.junit.Test;
+
+public class SlackWebhookServiceTest {
+
+  /**
+   * Test if received event have correct event type
+   */
+  @Test
+  public void eventProcessingTest() {
+
+  }
+
+  /**
+   * Test issue event processing using reflection because issueEventProcessing() method in SlackWebhookService is private
+   */
+  @Test
+  public void issueEventProcessingTest() {
+
+  }  
+
+  /**
+   * Test issue event processing using reflection because commentEventProcessing() method in SlackWebhookService is private
+   */
+
+  @Test
+  public void commentEventProcessingTest() {
+
+  }
+
+  /**
+   * Test invokeSlackWebhook method using reflection because invokeSlackWebhook() method in SlackWebhookService is private
+   */
+  @Test
+  public void invokeSlackWebhookTest() {
+
+  }
+
+  /**
+   * Test getEventType() method using reflection because getEventType() method in SlackWebhookService is private
+   */
+  @Test
+  public void getEventTypeTest() {
+
+  }
+
+  /**
+   * Test getIssueTypeIconBasedOnIssueTypeName() method using reflection because getIssueTypeIconBasedOnIssueTypeName() method in SlackWebhookService is private
+   *
+   * In this method we are going to test if we are receive right issue type icon based on issue type name
+   */
+  @Test
+  public void getIssueTypeIconBasedOnIssueTypeNameTest() {
+
+  }
+
+  /**
+   * Test getIssueTypeIconBasedOnIssueTypeName() method using reflection because getIssueTypeIconBasedOnIssueTypeName() method in SlackWebhookService is private
+   *
+   * In this method we are going to test if we are receive right issue type icon based on issue type name and priorityName
+   */
+  @Test
+  public void getIssueTypeIconBasedOnIssueTypeNameAndIssuePriorityNameTest() {
+
+  }
+  /**
+   * Test getIssueTypeIconBasedOnIssueTypeName() method using reflection because getIssueTypeIconBasedOnIssueTypeName() method in SlackWebhookService is private
+   *
+   * In this method we are going to test if we are receive right issue type icon based on issue type name and priorityName
+   */
+  @Test
+  public void findMentionedUsernameListInTheCommentTest() {
+
+  }
+}
