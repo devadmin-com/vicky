33efdc666c48aa30ddd65ee7e021e7308af36d49
Victor Devadmin <victor@devadmin.com>
2018-12-19 18:29:54 +0000

    some more todos

--- vicky/src/main/java/com/devadmin/vicky/ChangeLogItem.java
+++ vicky/src/main/java/com/devadmin/vicky/ChangeLogItem.java
@@ -13,6 +13,7 @@ public interface ChangeLogItem {
   String getTo();
 
   /**
+   * @todo is this what we really want to do, or would be better off having a type enum like we do for TaskEventModel.getType?
    * @return true if the changelog is a task assignment (could be first assignment or reassignment)
    */
   boolean isAssign();
