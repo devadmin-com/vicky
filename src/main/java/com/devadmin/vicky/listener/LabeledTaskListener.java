151afa0b8adb0c2a275571bc26af2fb048d30c2b
sheishin <sheishin@devadmin.com>
2020-02-21 17:29:33 +0800

    TL-190

--- src/main/java/com/devadmin/vicky/listener/LabeledTaskListener.java
+++ src/main/java/com/devadmin/vicky/listener/LabeledTaskListener.java
@@ -6,7 +6,6 @@
 
 package com.devadmin.vicky.listener;
 
-import com.devadmin.vicky.*;
 import com.devadmin.vicky.event.TaskEventModelWrapper;
 import com.devadmin.vicky.format.TaskEventFormatter;
 import com.devadmin.vicky.model.jira.task.TaskEvent;
@@ -37,17 +36,23 @@ public class LabeledTaskListener extends TaskToMessageListener {
 
     @Override
     public void onApplicationEvent(TaskEventModelWrapper eventWrapper) {
-
         TaskEvent event = eventWrapper.getTaskEventModel();
-        if (event.getType() == TaskEventType.CREATED || event.getTask().isResolved()) {
-            for (String label : event.getTask().getLabels()) {
-                if (!this.shouldSkip(eventWrapper)) {
-                    log.info("Trying to send channel message about labeled task");
-                    messageService.sendChannelMessage(label, formatter.format(event));
-                } else {
-                    log.info("Event {} doesn't send notification", eventWrapper.getEventModel());
-                }
-            }
+
+        if (shouldListenerReactOnEvent(event, eventWrapper)) {
+            sendMessage(event);
+        } else {
+            log.info("Event {} doesn't send notification", eventWrapper.getEventModel());
+        }
+    }
+
+    private boolean shouldListenerReactOnEvent(TaskEvent event, TaskEventModelWrapper eventWrapper) {
+        return event.getType() == TaskEventType.CREATED || event.getTask().isResolved() && !this.shouldSkip(eventWrapper);
+    }
+
+    private void sendMessage(TaskEvent event) {
+        for (String label : event.getTask().getLabels()) {
+            log.info("Trying to send channel message about labeled task");
+            messageService.sendChannelMessage(label, formatter.format(event));
         }
     }
 }
