cf0b47e3b44f09bf09ebdcdfe9eaed5307de8758
strogiyotec <almas337519@gmail.com>
2020-02-21 16:08:41 -0800

    create task listener test

---
+++ src/test/java/com/devadmin/vicky/test/CreatedTaskListenerTest.java
@@ -0,0 +1,112 @@
+package com.devadmin.vicky.test;
+
+import com.devadmin.vicky.config.FormatConfig;
+import com.devadmin.vicky.event.TaskEventModelWrapper;
+import com.devadmin.vicky.format.SummaryTaskEventFormatter;
+import com.devadmin.vicky.format.TaskEventFormatter;
+import com.devadmin.vicky.listener.CreatedTaskListener;
+import com.devadmin.vicky.model.jira.JiraEventModel;
+import com.devadmin.vicky.model.jira.task.TaskEventType;
+import com.devadmin.vicky.service.slack.MessageService;
+import com.devadmin.vicky.service.slack.SlackMessageServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.devadmin.vicky.test.TestTasks.PROJECT;
+import static com.devadmin.vicky.test.TestTasks.taskModel;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.never;
+
+/**
+ * Test class for {@link CreatedTaskListener}
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        classes = {
+                FormatConfig.class,
+                CreatedTaskListener.class,
+                SummaryTaskEventFormatter.class,
+                SlackMessageServiceImpl.class,
+                ApplicationEventPublisher.class
+        }
+)
+public class CreatedTaskListenerTest {
+
+    /**
+     * Event publisher.
+     */
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    /**
+     * Event formatter to test.
+     * Add qualifier to avoid warning from IDEA
+     */
+    @Autowired
+    @Qualifier("SummaryFormatter")
+    private TaskEventFormatter eventFormatter;
+
+    /**
+     * Mocked slack message sender.
+     */
+    @MockBean
+    private MessageService messageService;
+
+    /**
+     * Test that channel gets notification.
+     */
+    @Test
+    public void testSendNotificationOnCreatedEvent() {
+        final JiraEventModel testEventModel = taskModel("serpento", "testUser", "Test task");
+        testEventModel.setType(TaskEventType.CREATED);
+        this.applicationEventPublisher.publishEvent(new TaskEventModelWrapper(testEventModel));
+        Mockito.verify(
+                this.messageService, atLeastOnce())
+                .sendChannelMessage(
+                        PROJECT,
+                        this.eventFormatter.format(testEventModel)
+                );
+    }
+
+    /**
+     * Test that UPDATED type will be skipped.
+     */
+    @Test
+    public void testWrongEventType() {
+        final JiraEventModel testEventModel = taskModel("serpento", "testUser", "Test task");
+        testEventModel.setType(TaskEventType.UPDATED);
+        this.applicationEventPublisher.publishEvent(new TaskEventModelWrapper(testEventModel));
+        Mockito.verify(
+                this.messageService, never())
+                .sendChannelMessage(
+                        any(),
+                        any()
+                );
+    }
+
+    /**
+     * Test that only issues with id 13 are supported.
+     */
+    @Test
+    public void testSkipIssueId() {
+        final JiraEventModel testEventModel = taskModel("serpento", "testUser", "Test task");
+        testEventModel.getIssue().getFields().getIssueType().setId("228");
+        this.applicationEventPublisher.publishEvent(new TaskEventModelWrapper(testEventModel));
+        Mockito.verify(
+                this.messageService, never())
+                .sendChannelMessage(
+                        any(),
+                        any()
+                );
+    }
+
+
+}
--- src/test/java/com/devadmin/vicky/test/TestTasks.java
+++ src/test/java/com/devadmin/vicky/test/TestTasks.java
@@ -1,9 +1,6 @@
 package com.devadmin.vicky.test;
 
-import com.devadmin.vicky.model.jira.AuthorModel;
-import com.devadmin.vicky.model.jira.FieldModel;
-import com.devadmin.vicky.model.jira.JiraEventModel;
-import com.devadmin.vicky.model.jira.UserModel;
+import com.devadmin.vicky.model.jira.*;
 import com.devadmin.vicky.model.jira.comment.CommentModel;
 import com.devadmin.vicky.model.jira.status.StatusModel;
 import com.devadmin.vicky.model.jira.task.IssueModel;
@@ -13,6 +10,8 @@ import com.devadmin.vicky.model.jira.task.IssueTypeModel;
  * Create test tasks.
  */
 public final class TestTasks {
+
+    public static final String PROJECT = "vicky";
 
     public static JiraEventModel taskModel(final String creator, final String assignee, final String body) {
         final AuthorModel authorModel = new AuthorModel();
@@ -30,23 +29,28 @@ public final class TestTasks {
 
         final StatusModel statusModel = new StatusModel();
         statusModel.setName("test");
+        statusModel.setDescription("Bla bla");
 
         final UserModel userModel = new UserModel();
         userModel.setName(assignee);
         userModel.setDisplayName(assignee);
         userModel.setEmailAddress(assignee);
 
+        final ProjectModel projectModel = new ProjectModel();
+        projectModel.setName(PROJECT);
+
         final FieldModel fieldModel = new FieldModel();
         fieldModel.setIssueType(issueTypeModel);
         fieldModel.setStatus(statusModel);
         fieldModel.setAssignee(userModel);
+        fieldModel.setProject(projectModel);
         final IssueModel issueModel = new IssueModel();
         issueModel.setId("13");
         issueModel.setFields(fieldModel);
         JiraEventModel testEventModel = new JiraEventModel();
         testEventModel.setComment(comment);
         testEventModel.setIssue(issueModel);
-
+        testEventModel.setTimeStamp(System.currentTimeMillis());
         return testEventModel;
     }
 }
